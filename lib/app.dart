6e4549387505fee0108b43c0bbbf52f6d0dd904b
Surat Adhikari <surajadkhari@gmail.com>
2022-08-01 13:25:02 +0545

    go_router object _route

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:go_router/go_router.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    final router = GoRouter(routes: []);
+    return const MaterialApp();
   }
 }
